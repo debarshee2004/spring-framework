4f1f79359a6d5b6329b6a21b28fc9e06294f5715
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-11-21 07:32:51 +0530

    Added information about dependence injection and autowiring

--- spring-core/src/main/java/com/debarshee/main/MainApplication.java
+++ spring-core/src/main/java/com/debarshee/main/MainApplication.java
@@ -2,12 +2,18 @@ package com.debarshee.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MainApplication.class, args);
+		System.out.println("This is a Spring Core Application using Spring Boot.");
+
+//		Dependence Injection
+		Alien object = context.getBean(Alien.class);
+		object.information();
 	}
 
 }
