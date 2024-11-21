1caac4d4b4b3b78c9de84aac2aef35e2b635e2d4
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-11-21 11:27:53 +0530

    Updated Maven Project

---
+++ maven-project-scratch/src/main/java/org/debarshee/Alien.java
@@ -0,0 +1,7 @@
+package org.debarshee;
+
+public class Alien {
+    public void information() {
+        System.out.println("Hello, I am an alien form outside for your galaxy.");
+    }
+}
--- maven-project-scratch/src/main/java/org/debarshee/Main.java
+++ maven-project-scratch/src/main/java/org/debarshee/Main.java
@@ -1,7 +1,25 @@
 package org.debarshee;
 
+//import org.springframework.beans.factory.BeanFactory;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
+//import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a Maven Project.");
+
+        /* BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+
+        Alien object = (Alien) factory.getBean("alien");
+        object.information(); */
+
+//      This is a Singleton object. That means it will only create one object per class.
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Alien object = (Alien) context.getBean("alien");
+        object.information();
     }
 }
