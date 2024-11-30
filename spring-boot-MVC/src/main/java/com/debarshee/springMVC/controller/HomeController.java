a77ebc8bb851ae6f87e74c2540aaa613eb4be5d5
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-11-30 21:33:16 +0530

    Added basic ModelandView Controller Working

--- spring-boot-MVC/src/main/java/com/debarshee/springMVC/controller/HomeController.java
+++ spring-boot-MVC/src/main/java/com/debarshee/springMVC/controller/HomeController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -11,20 +13,32 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         System.out.println("Home page was requested...");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String result(HttpServletRequest req) {
-        int i = Integer.parseInt(req.getParameter("num1"));
-        int j = Integer.parseInt(req.getParameter("num2"));
+    public ModelAndView result(
+//            HttpServletRequest req
+            @RequestParam("num1") int i,
+            @RequestParam("num2") int j
+//            HttpSession session
+    ) {
+//        int i = Integer.parseInt(req.getParameter("num1"));
+//        int j = Integer.parseInt(req.getParameter("num2"));
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("result");
 
         int sum = i+j;
 
-        HttpSession session = req.getSession();
-        session.setAttribute("sum", sum);
+//        HttpSession session = req.getSession();
+//        session.setAttribute("sum", sum);
+
+        modelAndView.addObject("sum", sum);
 
         System.out.println("Result was requested...");
-        return "result.jsp";
+
+//        return "result.jsp";
+        return modelAndView;
     }
 }
