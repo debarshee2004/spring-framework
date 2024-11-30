3885eb999808f577936255beab51099f3a1a40c1
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-11-30 21:17:19 +0530

    Added basic Servlet Controller Working

--- spring-boot-MVC/src/main/java/com/debarshee/springMVC/controller/HomeController.java
+++ spring-boot-MVC/src/main/java/com/debarshee/springMVC/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.debarshee.springMVC.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
-    public void home() {
-        System.out.println("Home Page Requested...");
+    public String home() {
+        System.out.println("Home page was requested...");
+        return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String result(HttpServletRequest req) {
+        int i = Integer.parseInt(req.getParameter("num1"));
+        int j = Integer.parseInt(req.getParameter("num2"));
+
+        int sum = i+j;
+
+        HttpSession session = req.getSession();
+        session.setAttribute("sum", sum);
+
+        System.out.println("Result was requested...");
+        return "result.jsp";
     }
 }
---
+++ spring-boot-MVC/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>JSP Boilerplate</title>
+</head>
+<body>
+    <h1>Welcome to Index.jsp Home Page</h1>
+
+    <form action="add">
+        Enter 1st Number: <input type="text" name="num1"><br>
+        Enter 2nd Number: <input type="text" name="num2"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
