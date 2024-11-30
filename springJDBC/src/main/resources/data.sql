94ed82c719f5c13be3adf9cb302e6c8805611634
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-11-30 21:35:30 +0530

    update

--- springJDBC/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO ALIEN (id, name, tech) VALUES (101, 'Kunal', 'IOT');
-INSERT INTO ALIEN (id, name, tech) VALUES (102, 'Harsh', 'IOT');
-INSERT INTO ALIEN (id, name, tech) VALUES (103, 'Rohit', 'IOT');
