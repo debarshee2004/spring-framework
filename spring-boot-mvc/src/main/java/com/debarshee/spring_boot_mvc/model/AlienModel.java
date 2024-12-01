071d2fa4630649cc7c97a0433bdfc7ff4709c881
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-12-01 13:12:59 +0530

    added model and controller to spring boot mvc project

---
+++ spring-boot-mvc/src/main/java/com/debarshee/spring_boot_mvc/model/AlienModel.java
@@ -0,0 +1,80 @@
+package com.debarshee.spring_boot_mvc.model;
+
+/**
+ * Represents a model class for an Alien entity in a Spring MVC application.
+ * This class contains properties for an Alien's ID and name,
+ * along with getter and setter methods for accessing and modifying these properties.
+ */
+public class AlienModel {
+
+    // Unique identifier for the Alien
+    private int alien_id;
+
+    // Name of the Alien
+    private String alien_name;
+
+    /**
+     * Creates an empty controller of the Alien.
+     */
+    public AlienModel() {}
+
+    /**
+     * Gets the unique identifier of the Alien.
+     *
+     * @param alien_id the alien_id of the Alien.
+     * @param alien_name the alien_name of the Alien.
+     */
+    public AlienModel(int alien_id, String alien_name) {
+        this.alien_id = alien_id;
+        this.alien_name = alien_name;
+    }
+
+    /**
+     * Gets the unique identifier of the Alien.
+     *
+     * @return the alien_id of the Alien.
+     */
+    public int getAlien_id() {
+        return alien_id;
+    }
+
+    /**
+     * Sets the unique identifier of the Alien.
+     *
+     * @param alien_id the ID to set for the Alien.
+     */
+    public void setAlien_id(int alien_id) {
+        this.alien_id = alien_id;
+    }
+
+    /**
+     * Gets the name of the Alien.
+     *
+     * @return the alien_name of the Alien.
+     */
+    public String getAlien_name() {
+        return alien_name;
+    }
+
+    /**
+     * Sets the name of the Alien.
+     *
+     * @param alien_name the name to set for the Alien.
+     */
+    public void setAlien_name(String alien_name) {
+        this.alien_name = alien_name;
+    }
+
+    /**
+     * Provides a string representation of the AlienModel object.
+     *
+     * @return a string containing the Alien's ID and name.
+     */
+    @Override
+    public String toString() {
+        return "AlienModel{" +
+                "alien_id=" + alien_id +
+                ", alien_name='" + alien_name + '\'' +
+                '}';
+    }
+}
---
+++ spring-boot-mvc/src/main/webapp/view/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+</head>
+<body>
+    <form action="addAlien">
+        Enter your id: <input type="text" name="alien_id"><br>
+        Enter your name: <input type="text" name="alien_name"><br>
+        <input type="submit">
+    </form>
+    <hr>
+    <form action="getAlien" method="get">
+    	Enter your id : <input type="text" name="aid"><br>
+    </form>
+</body>
+</html>
