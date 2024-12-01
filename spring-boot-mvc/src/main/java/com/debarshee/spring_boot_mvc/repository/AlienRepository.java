a84c8db0820040401d98b3686f5d06c859105005
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-12-01 13:07:16 +0530

    created a new spring boot mvc project

--- spring-boot-mvc/src/main/java/com/debarshee/spring_boot_mvc/repository/AlienRepository.java
+++
@@ -1,7 +0,0 @@
-package com.debarshee.spring_boot_mvc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.debarshee.spring_boot_mvc.model.AlienModel;
-
-public interface AlienRepository extends JpaRepository<AlienModel, Integer>{}
