445bbca8e2a79ac6cc56bb188b67288bcc025595
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-12-02 09:20:24 +0530

    Created a get-alien restful api

--- spring-boot-restapi/src/main/java/com/debarshee/spring_boot_restapi/controller/AlienController.java
+++ spring-boot-restapi/src/main/java/com/debarshee/spring_boot_restapi/controller/AlienController.java
@@ -3,28 +3,31 @@ package com.debarshee.spring_boot_restapi.controller;
 import com.debarshee.spring_boot_restapi.model.AlienModel;
 import com.debarshee.spring_boot_restapi.repository.AlienRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class AlienController {
 
     @Autowired
     AlienRepository repository;
 
-    public void addAlien(@ModelAttribute("alien") AlienModel alienModel) {}
+    @PostMapping("alien")
+    public AlienModel addAlien(AlienModel alienModel) {
+        repository.save(alienModel);
+        return alienModel;
+    }
 
     @GetMapping("aliens")
-    @ResponseBody
     public List<AlienModel> getAliens() {
         List<AlienModel> aliens = repository.findAll();
         return aliens;
     }
 
-    public void getAlien(@RequestParam int alien_id) {}
+    @GetMapping("alien/{alien_id}")
+    public AlienModel getAlien(@PathVariable("alien_id") int alien_id) {
+        AlienModel alien = repository.findById(alien_id).orElse(new AlienModel(0, ""));
+        return alien;
+    }
 }
